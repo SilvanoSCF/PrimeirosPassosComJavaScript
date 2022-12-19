580c98736b989a7cf62b94ee5a1cc63f8d99aa44
SilvanoSCF <sscf.prog@gmail.com>
2022-12-19 13:52:45 -0300

    Estrutura condicional simples if else

--- Primeiros_passos/02 - Condicionais/Aula.js
+++ Primeiros_passos/02 - Condicionais/Aula.js
@@ -1,5 +1,37 @@
-const eAzul = true;
-const evermelho = false;
+console.log("Atribuindo 10 á constante numero.")
 
-10 >= 10;
+const numero = 12222245;
 
+const NumeroPar = (numero % 2) === 0;
+
+console.log("Resultado:  " + NumeroPar);
+
+if(NumeroPar){
+    console.log('par');
+}// Caso sendo par, a condição executara o log
+
+if(!NumeroPar){// A exclamação '!' ira inverter a condição 'not'
+    console.log('impar');
+}//Caso não seja par, a condição ira executar este codigo
+
+
+// Exemplo utilizando if / else //////////////////////////////////////////////
+
+if(NumeroPar){
+    console.log(numero+ ' é um numero par');
+}else{
+    console.log(numero+ ' é um numero impar');
+}
+
+// Exemplo de if/elseif/else
+
+const nDiv5 = (numero % 5) === 0;
+
+if (numero === 0){
+    console.log('O numero é invalido.')
+}else if (nDiv5)
+{
+    console.log(numero+ ' é divisivel por 5');
+}else{
+    console.log(numero+ ' não é divisivel por 5.')
+}
