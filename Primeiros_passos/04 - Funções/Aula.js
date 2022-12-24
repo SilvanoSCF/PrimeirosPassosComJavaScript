c4624ee782e3d06208e56c71824d7480e934b9cd
SilvanoSCF <sscf.prog@gmail.com>
2022-12-24 16:57:39 -0300

    Git Atualização

---
+++ Primeiros_passos/04 - Funções/Aula.js
@@ -0,0 +1,23 @@
+ // Main - Opder das Funções na Qualidade do seu código
+
+ function calcularImc(peso, altura){
+    // return peso / Math.pow(altura, 2);    
+ }
+
+
+ function classificarImc(imc){
+
+ }
+
+
+ function maim(){
+    const peso = 75;
+    const altura = 1.78;
+    const imc = calcularImc(peso, altura)
+    console.log(classificarImc(imc));
+ }
+
+ main = function(){
+    console.log(1);
+ }
+ main();
---
+++ Primeiros_passos/04 - Funções/Aula2.js
@@ -0,0 +1,5 @@
+function myname(nome){
+    console.log(nome);
+}
+
+myname('Silvano');
